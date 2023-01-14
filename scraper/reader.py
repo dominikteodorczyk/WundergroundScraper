8e3a544d2c126e16495a52945f240eb37c22f6a2
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-01-14 19:13:50 +0100

    adding DayPage class with date string from datetime object generator

--- scraper/reader.py
+++ scraper/reader.py
@@ -1,15 +1,18 @@
 from selenium import webdriver
+from datetime import datetime
 
 
 class SiteObject():
 
-    def __init__(self, site_path):
+    def __init__(self, site_path, start, end):
         '''
         Klasa inicjalizująca object drive selenium
         '''
         self.chromedriver = r'scraper\chromedriver.exe'
         self.driver = webdriver.Chrome(self.chromedriver)
         self.driver.get(site_path)
+        self.start = start
+        self.end = end
 
     def getHistorySite(self):
 
@@ -19,3 +22,21 @@ class SiteObject():
             if href.startswith('https://www.wunderground.com/history/'):
                 print(href)
                 i.click()
+
+    def iteratorOfDays():
+        pass
+
+class DayPage():
+
+    def __init__(self, date):
+        self.date = date
+
+    def strDateGenerator(self):
+
+        return self.date.strftime("%Y-%m-%d")
+
+    def hrfDayPage(sefl):
+
+        return 
+
+    
