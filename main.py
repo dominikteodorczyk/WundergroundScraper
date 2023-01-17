6b93aebf99322f9d337be0e2126f60f8773191e4
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-01-17 23:48:54 +0100

    Working day scraper module

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from scraper.reader import SiteObject
 from time import sleep
 from datetime import datetime
 
-SiteObject('https://www.wunderground.com/weather/pl/bobrowniki/IBOBRO8',"2020-08-28","2020-09-01").getData()
+SiteObject('https://www.wunderground.com/weather/de/bad-k%C3%B6tzting/IBAVARIA33',"2020-08-28","2020-09-01").getData()
